62e22fd16dc4f482fc56c9cb64801c247f5a9ee7
KutayGuler <s7019@horyzont.eu>
2025-02-16 16:07:07 +0300

    downgraded to tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				base: colors.zinc,
+				primary: colors.emerald,
+				secondary: colors.amber
+			}
+		}
+	},
+	plugins: []
+};
